bf2109620ed491f1c86993e6a48aa250c51a814f
Mi-lex <mishin-lex@mail.ru>
2018-11-27 16:55:38 +0300

    Add webp screens of projects

--- src/js/assets/imgs.js
+++ src/js/assets/imgs.js
@@ -1,3 +1,15 @@
-import './../../img/me-desktop.jpg';
-import './../../img/me-desktop(2x).jpg';
-import './../../img/me-mobile(2x).jpg';
+import './../../img/me-desktop@1x.webp';
+import './../../img/me-desktop@2x.webp';
+import './../../img/me-tablet@1x.webp';
+import './../../img/me-tablet@2x.webp';
+
+import './../../img/me-desktop@1x.jpg';
+import './../../img/me-desktop@2x.jpg';
+import './../../img/me-tablet@1x.jpg';
+import './../../img/me-tablet@2x.jpg';
+import './../../img/me-mobile@2x.jpg';
+
+import './../../img/projects/technomart.webp';
+import './../../img/projects/mishka.webp';
+import './../../img/projects/keksobooking.webp';
+import './../../img/projects/pixelhunter.webp';
