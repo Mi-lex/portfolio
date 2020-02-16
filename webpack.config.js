d4ab7fbe2106cee246a844c12ef6a99a9328499d
Mi-lex <mishin-lex@mail.ru>
2020-02-16 03:45:17 +0300

    replace deprecated package

--- webpack.config.js
+++ webpack.config.js
@@ -1,194 +1,185 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 
-const BUILD_FOLDER = 'docs'
+const BUILD_FOLDER = 'docs';
 
 const config = {
-    entry: {
-        app: [
-            './src/js/main.js',
-            './src/sass/style.scss'
-        ]
-    },
+	entry: {
+		app: ['./src/js/main.js', './src/sass/style.scss'],
+	},
 
-    output: {
-        path: path.resolve(__dirname, `./${BUILD_FOLDER}`),
-        filename: '[name].js'
-    },
+	output: {
+		path: path.resolve(__dirname, `./${BUILD_FOLDER}`),
+		filename: '[name].js',
+	},
 
-    module: {
-        rules: [
-            { 
-                test: /\.html$/, 
-                loader: 'html-loader',
-                options: {
-                    interpolate: true
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: [
-                    path.resolve(__dirname, './node_modules'),
-                    path.resolve(__dirname, './src/js/static'),
-                ],
-                loader: "babel-loader"
-            },
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, "./src/js/static"),
-                loaders: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: './static/[name].[ext]'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ['css-loader', 'postcss-loader']
-                })
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        'css-loader',
-                        'postcss-loader',
-                        'sass-loader'
-                    ]
-                })
-            },
-            {
-                test: /\.eot|ttf|woff2?$/,
-                loader: 'file-loader',
-                options: {
-                    name: './fonts/[name].[ext]'
-                }
-            },
-            {
-                test: /\.(png|jpe?g|gif|ico)$/,
-                loaders: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: './img/[name].[ext]'
-                        }
-                    },
-                ],
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: 'svg-sprite-loader',
-                        options: {
-                            extract: true,
-                            publicPath: '/img/'
-                        }
-                    },
-                    'svgo-loader'
-                ]
-            }
-        ]
-    },
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+				options: {
+					interpolate: true,
+				},
+			},
+			{
+				test: /\.js$/,
+				exclude: [
+					path.resolve(__dirname, './node_modules'),
+					path.resolve(__dirname, './src/js/static'),
+				],
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, './src/js/static'),
+				loaders: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: './static/[name].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.eot|ttf|woff2?$/,
+				loader: 'file-loader',
+				options: {
+					name: './fonts/[name].[ext]',
+				},
+			},
+			{
+				test: /\.(png|jpe?g|gif|ico)$/,
+				loaders: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: './img/[name].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: 'svg-sprite-loader',
+						options: {
+							extract: true,
+							publicPath: '/img/',
+						},
+					},
+					'svgo-loader',
+				],
+			},
+		],
+	},
+	devServer: {
+		contentBase: path.join(__dirname, BUILD_FOLDER),
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new SpriteLoaderPlugin(),
+		new ImageminWebpWebpackPlugin({
+			config: [
+				{
+					// exclude backgrounds images
+					test: /^(?:(?!bg-).)*\.(png|jpe?g)$/,
+					options: {
+						quality: 80,
+					},
+				},
+			],
+			overrideExtension: true,
+			detailedLogs: false,
+			silent: false,
+			strict: true,
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/html/main.html.js',
+		}),
+	],
 
-    plugins: [
-        new ExtractTextPlugin("[name].css"),
-        new SpriteLoaderPlugin(),
-        new ImageminWebpWebpackPlugin({
-            config: [{
-                // exclude backgrounds images
-                test: /^(?:(?!bg-).)*\.(png|jpe?g)$/,
-                options: {
-                    quality:  80
-                }
-            }],
-            overrideExtension: true,
-            detailedLogs: false,
-            silent: false,
-            strict: true
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/html/main.html.js'
-        }),
-    ],
-
-    optimization: {
-        minimizer: []
-    }
+	optimization: {
+		minimizer: [],
+	},
 };
 
 module.exports = (env, argv) => {
+	if (argv.mode === 'production') {
+		/**
+		 * Change rule for images
+		 * this is not the best approach
+		 * just don't want make another
+		 * config due to only one rule
+		 */
+		// the index of imgs rule is one that before last
+		const indexOfImgRules = config.module.rules.length - 2;
 
-    if (argv.mode === 'production') {
-        /**
-         * Change rule for images
-         * this is not the best approach
-         * just don't want make another
-         * config due to only one rule
-         */
-        // the index of imgs rule is one that before last
-        const indexOfImgRules = config.module.rules.length - 2;
+		config.module.rules[indexOfImgRules] = {
+			test: /\.(png|jpe?g|gif|ico)$/,
+			loaders: [
+				{
+					loader: 'file-loader',
+					options: {
+						name: './img/[name].[ext]',
+					},
+				},
+				{
+					loader: 'image-webpack-loader',
+					options: {
+						mozjpeg: {
+							progressive: true,
+							quality: 80,
+						},
+						optipng: {
+							enabled: false,
+						},
+						pngquant: {
+							quality: '80-90',
+							speed: 2,
+						},
+					},
+				},
+			],
+		};
 
-        config.module.rules[indexOfImgRules] = {
-            test: /\.(png|jpe?g|gif|ico)$/,
-            loaders: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: './img/[name].[ext]'
-                    }
-                },
-                {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      mozjpeg: {
-                        progressive: true,
-                        quality: 80
-                      },
-                      optipng: {
-                        enabled: false,
-                      },
-                      pngquant: {
-                        quality: '80-90',
-                        speed: 2
-                      },
-                    }
-                  },
-            ],
-        }
+		// Plagins
+		config.plugins.unshift(
+			new CleanWebpackPlugin([BUILD_FOLDER], {
+				root: __dirname,
+				verbose: true,
+				dry: false,
+			}),
+		);
+		config.plugins.push(
+			new webpack.LoaderOptionsPlugin({
+				minimize: true,
+			}),
+		);
+		// Minimizing
+		config.optimization.minimizer = [new UglifyJsPlugin()];
+	}
 
-        // Plagins
-        config.plugins.unshift(
-            new CleanWebpackPlugin([BUILD_FOLDER],
-                {
-                    root: __dirname,
-                    verbose: true,
-                    dry: false
-                }
-            )
-        )
-        config.plugins.push(
-            new webpack.LoaderOptionsPlugin({
-                minimize: true,
-            })
-        );
-        // Minimizing
-        config.optimization.minimizer = [
-            new UglifyJsPlugin()
-        ]
-    }
-
-    return config;
+	return config;
 };
