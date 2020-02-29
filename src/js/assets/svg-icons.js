b2a082265d25cc091bc0eb267a962211b5faf48a
Mi-lex <mishin-lex@mail.ru>
2020-02-29 14:42:53 +0300

    add one more project

--- src/js/assets/svg-icons.js
+++ src/js/assets/svg-icons.js
@@ -4,28 +4,31 @@ import './../../img/icons/web.svg';
 import './../../img/icons/html-5.svg';
 import './../../img/icons/css3.svg';
 import './../../img/icons/javascript.svg';
-import "./../../img/icons/php.svg";
-import "./../../img/icons/sass.svg";
-import "./../../img/icons/bem.svg";
-import "./../../img/icons/es6.svg";
-import "./../../img/icons/mysql.svg";
-import "./../../img/icons/laravel.svg";
-import "./../../img/icons/gulp.svg";
+import './../../img/icons/php.svg';
+import './../../img/icons/sass.svg';
+import './../../img/icons/bem.svg';
+import './../../img/icons/bootstrap.svg';
+import './../../img/icons/es6.svg';
+import './../../img/icons/mysql.svg';
+import './../../img/icons/laravel.svg';
+import './../../img/icons/gulp.svg';
+import './../../img/icons/react.svg';
+import './../../img/icons/typescript.svg';
 
 // Tools
-import "./../../img/icons/vscode.svg";
-import "./../../img/icons/git.svg";
-import "./../../img/icons/figma.svg";
-import "./../../img/icons/chromedev.svg";
+import './../../img/icons/vscode.svg';
+import './../../img/icons/git.svg';
+import './../../img/icons/figma.svg';
+import './../../img/icons/webpack.svg';
 
 // Project-panel
-import "./../../img/icons/safari-panel.svg";
+import './../../img/icons/safari-panel.svg';
 
 // Contacts
-import "./../../img/icons/phone.svg";
-import "./../../img/icons/mail.svg";
+import './../../img/icons/phone.svg';
+import './../../img/icons/mail.svg';
 
 // Social
-import "./../../img/icons/vk.svg";
-import "./../../img/icons/fb.svg";
-import "./../../img/icons/github.svg";
+import './../../img/icons/vk.svg';
+import './../../img/icons/fb.svg';
+import './../../img/icons/github.svg';
